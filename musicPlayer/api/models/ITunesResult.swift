332ab249aab754fb78cd47e0af62fac7060277ca
exsertin <exsertin@yandex.ru>
2020-07-19 22:26:00 +0300

    feature(api): new model ITunesResult

---
+++ musicPlayer/api/models/ITunesResult.swift
@@ -0,0 +1,21 @@
+//
+//  ITunesResult.swift
+//  musicPlayer
+//
+//  Created by Александр Левин on 19.07.2020.
+//  Copyright © 2020 Exsertin. All rights reserved.
+//
+
+struct ITunesResult: Decodable {
+  struct Result: Decodable {
+    var kind: String
+    var artistName: String
+    var collectionName: String
+    var trackName: String
+    var previewUrl: String
+    var artworkUrl100: String
+  }
+  
+  var resultCount: Int
+  var results: [Result]
+}
