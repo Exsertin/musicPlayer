303b0f4a5867b06deb82750cd2def603fe301afe
exsertin <exsertin@yandex.ru>
2020-07-19 22:00:37 +0300

    feature(controllers): new SearchTableViewController

---
+++ musicPlayer/controllers/SearchTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchTableViewController.swift
+//  musicPlayer
+//
+//  Created by Александр Левин on 19.07.2020.
+//  Copyright © 2020 Exsertin. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
