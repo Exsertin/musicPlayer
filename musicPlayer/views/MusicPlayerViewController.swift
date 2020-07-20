013ff41ad78db0efa8cfcc1ab94a4204c34cdf96
exsertin <exsertin@yandex.ru>
2020-07-20 08:52:44 +0300

    feature(views): new view MusicPlayerViewController

---
+++ musicPlayer/views/MusicPlayerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerViewController.swift
+//  musicPlayer
+//
+//  Created by Александр Левин on 20.07.2020.
+//  Copyright © 2020 Exsertin. All rights reserved.
+//
+
+import UIKit
+
+class MusicPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
