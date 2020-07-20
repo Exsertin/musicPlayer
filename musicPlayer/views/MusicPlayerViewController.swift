34babf340c15e18610738f94fa8f325bb2e59db3
exsertin <exsertin@yandex.ru>
2020-07-20 13:47:41 +0300

    feature(views): add new @IBOutlet and @IBAction for music player

--- musicPlayer/views/MusicPlayerViewController.swift
+++ musicPlayer/views/MusicPlayerViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class MusicPlayerViewController: UIViewController {
+  @IBOutlet weak var artworkImageView: UIImageView!
+  @IBOutlet weak var artistNameLabel: UILabel!
+  @IBOutlet weak var trackNameLabel: UILabel!
+  @IBOutlet weak var collectionNameLabel: UILabel!
+  @IBOutlet weak var slider: UISlider!
+  @IBOutlet weak var currentTimeLabel: UILabel!
+  @IBOutlet weak var endTimeLabel: UILabel!
+  
+  
   var presenter: MusicPlayerPresenterProtocol!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+  }
+  
+  @IBAction func playPauseAction(_ sender: UIButton) {
   }
 }
