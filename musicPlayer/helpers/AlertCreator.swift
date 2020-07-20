720a75b51feb81530aee29809be6a6d26c94e5f2
exsertin <exsertin@yandex.ru>
2020-07-20 22:13:20 +0300

    chore(helpers): remove extra comments from AlertCreator

--- musicPlayer/helpers/AlertCreator.swift
+++ musicPlayer/helpers/AlertCreator.swift
@@ -10,9 +10,7 @@ import UIKit
 
 class AlertCreator {
  static func notification(message: String) -> UIAlertController {
-    // create the alert
     let alert = UIAlertController(title: "Notification", message: message, preferredStyle: UIAlertController.Style.alert)
-    // add an action (button)
     alert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
     
     return alert
