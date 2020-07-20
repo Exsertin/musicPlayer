aaabc2a4f2db3a037529e2d671794ff34ce64e99
exsertin <exsertin@yandex.ru>
2020-07-20 09:25:05 +0300

    feature(protocols): new protocol MusicPlayerViewDelegate

---
+++ musicPlayer/protocols/MusicPlayerViewDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  MusicPlayerViewDelegate.swift
+//  musicPlayer
+//
+//  Created by Александр Левин on 20.07.2020.
+//  Copyright © 2020 Exsertin. All rights reserved.
+//
+
+protocol MusicPlayerViewDelegate {
+  
+}
