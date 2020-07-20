305fbeb4b52c16a0f7a3e54667438d3fae008afa
exsertin <exsertin@yandex.ru>
2020-07-20 10:01:50 +0300

    chore(api): move Api, ApiManager and ApiResponseDecoder from api to api/protocols

--- musicPlayer/api/protocols/ApiManager.swift
+++ musicPlayer/api/protocols/ApiManager.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2020 Exsertin. All rights reserved.
 //
 
+import UIKit
+
 protocol ApiManager {
   func findSongs(searchText: String, completion: @escaping ([ITunesResult.Result]) -> Void)
+  func loadArtwork(url: String, completion: @escaping (Data) -> Void)
 }
