1e32e1f9905e158c56e28c204c2c277b7e8237ab
exsertin <exsertin@yandex.ru>
2020-07-21 18:51:53 +0300

    refactor(view): new func genViews

--- musicPlayer/views/MusicPlayerViewController.swift
+++ musicPlayer/views/MusicPlayerViewController.swift
@@ -7,16 +7,17 @@
 //
 
 import UIKit
+import SnapKit
 
 class MusicPlayerViewController: UIViewController {
-  @IBOutlet weak var artworkImageView: UIImageView!
-  @IBOutlet weak var artistNameLabel: UILabel!
-  @IBOutlet weak var trackNameLabel: UILabel!
-  @IBOutlet weak var collectionNameLabel: UILabel!
-  @IBOutlet weak var slider: UISlider!
-  @IBOutlet weak var currentTimeLabel: UILabel!
-  @IBOutlet weak var endTimeLabel: UILabel!
-  @IBOutlet weak var playPauseButton: UIButton!
+  weak var artworkImageView: UIImageView!
+  weak var artistNameLabel: UILabel!
+  weak var trackNameLabel: UILabel!
+  weak var collectionNameLabel: UILabel!
+  weak var slider: UISlider!
+  weak var currentTimeLabel: UILabel!
+  weak var endTimeLabel: UILabel!
+  weak var playPauseButton: UIButton!
   
   let playTitle = "Play"
   let pauseTitle = "Pause"
@@ -25,8 +26,113 @@ class MusicPlayerViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    genViews()
     presenter.setViewDelegate(viewDelegate: self)
     presenter.viewDidLoadDelegate()
+  }
+  
+  private func genViews() {
+    let image = UIImageView()
+    artworkImageView = image
+    artworkImageView.backgroundColor = .red
+    self.view.addSubview(artworkImageView)
+    artworkImageView.snp.makeConstraints { (make) -> Void in
+      make.top.equalTo(self.topLayoutGuide.snp.bottom).offset(30)
+      make.centerX.equalTo(self.view)
+      make.size.equalTo(CGSize(width: 100, height: 100))
+    }
+    
+    let lblArtist = UILabel()
+    lblArtist.text = "Artist:"
+    self.view.addSubview(lblArtist)
+    lblArtist.snp.makeConstraints { (make) -> Void in
+      make.top.equalTo(self.artworkImageView.snp_bottom).offset(39)
+      make.leading.equalTo(self.view).offset(8)
+    }
+    
+    let lblArtistName = UILabel()
+    artistNameLabel = lblArtistName
+    artistNameLabel.text = "Artist:"
+    self.view.addSubview(artistNameLabel)
+    artistNameLabel.snp.makeConstraints { (make) -> Void in
+      make.top.equalTo(self.artworkImageView.snp_bottom).offset(39)
+      make.leading.equalTo(lblArtist.snp_trailing).offset(8)
+    }
+    
+    let lblTrack = UILabel()
+    lblTrack.text = "Track:"
+    self.view.addSubview(lblTrack)
+    lblTrack.snp.makeConstraints { (make) -> Void in
+      make.top.equalTo(lblArtist.snp_bottom).offset(10)
+      make.leading.equalTo(self.view).offset(8)
+    }
+    
+    let lblTrackName = UILabel()
+    trackNameLabel = lblTrackName
+    trackNameLabel.text = "Track:"
+    self.view.addSubview(trackNameLabel)
+    trackNameLabel.snp.makeConstraints { (make) -> Void in
+      make.top.equalTo(self.artistNameLabel.snp_bottom).offset(10)
+      make.leading.equalTo(lblTrack.snp_trailing).offset(8)
+    }
+    
+    let lblCollection = UILabel()
+    lblCollection.text = "Collection:"
+    self.view.addSubview(lblCollection)
+    lblCollection.snp.makeConstraints { (make) -> Void in
+      make.top.equalTo(lblTrack.snp_bottom).offset(10)
+      make.leading.equalTo(self.view).offset(8)
+    }
+    
+    let lblCollectionName = UILabel()
+    collectionNameLabel = lblCollectionName
+    collectionNameLabel.text = "Collection:"
+    self.view.addSubview(collectionNameLabel)
+    collectionNameLabel.snp.makeConstraints { (make) -> Void in
+      make.top.equalTo(self.trackNameLabel.snp_bottom).offset(10)
+      make.leading.equalTo(lblCollection.snp_trailing).offset(8)
+    }
+    
+    let btnPlay = UIButton()
+    btnPlay.setTitle(playTitle, for: .normal)
+    btnPlay.setTitleColor(.blue, for: .normal)
+    playPauseButton = btnPlay
+    self.view.addSubview(playPauseButton)
+    playPauseButton.snp.makeConstraints { (make) -> Void in
+      make.centerX.equalTo(self.view)
+            make.size.equalTo(CGSize(width: 100, height: 100))
+      make.bottom.equalTo(self.bottomLayoutGuide.snp.top).offset(30)
+    }
+    
+    let sldLeftRightOffset = 20
+    let sldr = UISlider()
+    slider = sldr
+    self.view.addSubview(slider)
+    slider.snp_makeConstraints { (make) -> Void in
+      make.leading.equalTo(self.view).offset(sldLeftRightOffset)
+      make.trailing.equalTo(self.view).offset(-sldLeftRightOffset)
+      make.bottom.equalTo(self.playPauseButton.snp_top).offset(-40)
+    }
+    
+    let lblCurrentTime = UILabel()
+    lblCurrentTime.text = "currentTime"
+    currentTimeLabel = lblCurrentTime
+    self.view.addSubview(currentTimeLabel)
+    
+    currentTimeLabel.snp.makeConstraints {
+      $0.top.equalTo(self.slider.snp_bottom).offset(5)
+      $0.leading.equalTo(self.view).offset(sldLeftRightOffset)
+    }
+    
+    let lblEndTime = UILabel()
+    lblEndTime.text = "entTime"
+    endTimeLabel = lblEndTime
+    self.view.addSubview(endTimeLabel)
+    
+    endTimeLabel.snp.makeConstraints {
+      $0.top.equalTo(self.slider.snp_bottom).offset(5)
+      $0.trailing.equalTo(self.view).offset(-sldLeftRightOffset)
+    }
   }
   
   @IBAction func playPauseAction(_ sender: UIButton) {
