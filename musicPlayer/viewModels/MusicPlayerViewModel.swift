1cedc9005ac48da760a35de8707558f0c53719c4
exsertin <exsertin@yandex.ru>
2020-07-31 09:42:31 +0300

    feature(viewModels): new view model MusicPlayerViewModel and protocol MusicPlayerViewModelProtocol

---
+++ musicPlayer/viewModels/MusicPlayerViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  MusicPlayerViewModel.swift
+//  musicPlayer
+//
+//  Created by Александр Левин on 31.07.2020.
+//  Copyright © 2020 Exsertin. All rights reserved.
+//
+
+struct MusicPlayerViewModel: MusicPlayerViewModelProtocol {
+  
+}
